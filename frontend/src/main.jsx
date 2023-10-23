ea99488a6284c9a717f5275ec5cacbc71328c772
Rían Errity <rian@paradaux.io>
2023-10-23 20:31:12 +0100

    Setup styling

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import IndexPage from './pages/IndexPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <IndexPage/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ frontend/src/pages/IndexPage.jsx
@@ -0,0 +1,10 @@
+import "./IndexPage.scss"
+
+export default function IndexPage() {
+    return (
+        <div className="index-page">
+            <h1>Weather App</h1>
+
+        </div>
+    )
+}
